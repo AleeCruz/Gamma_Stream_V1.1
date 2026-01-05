8a72cac360dd908044b53170afcf2abd5b1f6557
Alexander <acruz@fi.uba.ar>
2026-01-05 19:40:49 -0300

    fix: Se arreglo el link de servivios

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Gamma_Stream_V1.1/', 
+  base: '/Gamma_Stream_V1.1/', // Asegúrate de que este sea el nombre de tu repo
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        servicios: resolve(__dirname, 'servicios.html'),
+      },
+    },
+  },
 })
