a06a4dc8987e110bbdbccce9197c5fc8211d6e37
Alexander <acruz@fi.uba.ar>
2026-01-05 20:14:31 -0300

    Se actualizo los elementos correspondientes

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
+
 const renderer = new THREE.WebGLRenderer();
+
+const container = document.querySelector('main')
+
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+container.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
