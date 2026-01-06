219a694022ed4719b4c0bc2932544c4102dcd089
Alexander <acruz@fi.uba.ar>
2026-01-05 21:29:24 -0300

    Se actualizo el css

--- main.js
+++ main.js
@@ -3,25 +3,13 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-
-
 const renderer = new THREE.WebGLRenderer();
-
-const container = document.querySelector('main')
-
-
-renderer.setSize( container.clientWidth, container.clientHeight);
-camera.aspect = container.clientWidth/container.clientHeight;
-camera.updateProjectionMatrix();
-
-
-
-
+renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
-container.appendChild(renderer.domElement);
+document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
